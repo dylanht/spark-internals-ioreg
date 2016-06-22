06a1bda2ecec3b1846b503d03fd1363066528271
dylanht <dylan.bethune.waddell@mail.utoronto.ca>
2016-06-22 11:20:41 -0400

    This project does not compile, though I exepect it to - it would seem that it fails with: /fs1/home/dbethune/pancure/ioregs/src/main/groovy/edu/juris/graph/ioregs/CompactBufferSerializer.groovy: 37: Can't have an abstract method in a non-abstract class. The class 'edu.juris.graph.ioregs.CompactBufferSerializer' must be declared abstract or the method 'void write(org.apache.tinkerpop.gremlin.structure.io.gryo.kryoshim.KryoShim, org.apache.tinkerpop.gremlin.structure.io.gryo.kryoshim.OutputShim, java.lang.Object)' must be implemented. --- as near as I can tell based on actually changing the method signature to not have CompactBuffer and rather just Object as the final parameter type in the overridden method, which is not a valid thing to do anyway and the compiler tells me that it wanted CompactBuffer to be there, the groovy compiler screws something up and what in the interface method is 'final T object' becomes just 'java.lang.Object' here. I was running into similar issues just trying to compile spark-gremlin after changing CompactBufferSerializer.groovy to implement SerializerShim.

---
+++ src/main/java/edu/juris/graph/ioregs/SparkInternalsIoRegistry.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package edu.juris.graph.ioregs;
+
+import org.apache.tinkerpop.gremlin.structure.io.AbstractIoRegistry;
+import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoIo;
+import org.apache.spark.util.collection.CompactBuffer;
+import edu.juris.graph.ioregs.CompactBufferSerializer;
+
+public final class SparkInternalsIoRegistry extends AbstractIoRegistry {
+
+    private static final SparkInternalsIoRegistry INSTANCE = new SparkInternalsIoRegistry();
+
+    private SparkInternalsIoRegistry() {
+	register(GryoIo.class, CompactBuffer.class, new CompactBufferSerializer());
+	register(GryoIo.class, CompactBuffer[].class, null);
+    }
+
+    public static SparkInternalsIoRegistry getInstance() {
+	return INSTANCE;
+    }   
+}
